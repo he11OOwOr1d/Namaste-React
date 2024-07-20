6e8a90a7b9ad50ceb99ecf6ef3c34f025618c8a8
Nikhil Raj <nikhilraj@Nikhils-MacBook-Pro.local>
2024-07-20 22:27:27 +0530

    Notes

---
+++ App.js
@@ -0,0 +1,9 @@
+
+const heading = React.createElement(
+  "div", 
+  {
+  id: "parent",
+  }, React.createElement("h1",{id:"child"},React.createElement("div",{id:"child1"},[React.createElement("h1",{},"h1"),React.createElement("h2",{},"h2"),React.createElement("h3",{},"h3")])))
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
